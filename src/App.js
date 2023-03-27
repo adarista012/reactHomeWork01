a80a9d59f35d483f23025b54af9baddb6ca5a509
antonio arista <ad.arista@acad.ucb.edu.bo>
2023-03-26 23:12:08 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import rey1 from './r1.png';
+import rey2 from './r2.png';
+import rey3 from './r3.png';
 import './App.css';
-
+const content = [
+  {
+    text: "rey1",
+    url: rey1
+  },
+  {
+    text: "rey2",
+    url: rey2
+  },
+  {
+    text: "rey3",
+    url: rey3
+  }
+]
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="main">
+      <div className="card">
+        <img src={content[0].url} className="imgContainer"/>
+        <h3>{content[0].text}</h3>
+      </div>
+      <div className="card">
+        <img src={content[1].url} className="imgContainer"/>
+        <h3>{content[1].text}</h3>
+      </div>
+      <div className="card">
+        <img src={content[2].url} className="imgContainer"/>
+        <h3>{content[2].text}</h3>
+      </div>
     </div>
   );
 }
